7f2fd6f9877facf6f967909c81b10a661ecba740
Filip Jerga <jerga99@gmail.com>
2021-03-08 08:53:49 +0000

    _app js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+
+function MainApp({Component, pageProps}) {
+  return (
+    <Component {...pageProps} />
+  )
 }
 
-export default MyApp
+export default MainApp;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+
+
+
+function About() {
+  return (
+    <h1>I am about page</h1>
+  )
+}
+
+
+export default About;
