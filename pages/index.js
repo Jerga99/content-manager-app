af6f9ba2f3c8f5c5be98819c1647b61a8be1ff23
Filip Jerga <jerga99@gmail.com>
2021-03-08 09:19:42 +0000

    Components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
 
-
-export default function Home() {
+function CompA(){
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>CompA</h1>
+      <p>Hello Comp A</p>
+      <CompB />
+    </>
   )
 }
+
+function CompB(){
+  return (
+    <>
+      <h1>CompB</h1>
+      <p>Hello Comp B</p>
+    </>
+  )
+}
+
+function Home() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <CompA />
+    </>
+  )
+}
+
+
+export default Home;
