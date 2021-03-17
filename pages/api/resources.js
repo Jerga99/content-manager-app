d8e4af7bb47698bd0a2cf2dce30af80ff5264589
Filip Jerga <jerga99@gmail.com>
2021-03-17 13:20:52 +0000

    Attach data in the request

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,8 +1,15 @@
 
 
 export default async function(req, res) {
-  const dataRes = await fetch("http://localhost:3001/api/resources");
-  const data = await dataRes.json();
+  if (req.method === "GET") {
+    const dataRes = await fetch("http://localhost:3001/api/resources");
+    const data = await dataRes.json();
 
-  res.send(data);
+    return res.send(data);
+  }
+
+  if (req.method === "POST") {
+    console.log(req.body);
+    return res.send("Data has been received!");
+  }
 }
