091fdb2b90f677d930b9c8891d0c0d35a311f50c
Filip Jerga <jerga99@gmail.com>
2021-03-17 12:02:03 +0000

    Resource form

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -6,8 +6,22 @@ const ResourceCreate = () => {
 
   return (
     <Layout>
-      <div>
-        I am resource create page
+      <div className="container">
+        <div className="columns">
+          <div className="column is-8 is-offset-2">
+            <div className="resource-form">
+              <h1 className="title">Add New Resource</h1>
+              <form>
+                <div className="field">
+                  <label className="label">Name</label>
+                  <div className="control">
+                    <input className="input" type="text" placeholder="Text input" />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   )
