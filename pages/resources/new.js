b60aa537df3f589ea165b47cb4eb2d69d72d38d4
Filip Jerga <jerga99@gmail.com>
2021-03-17 12:37:18 +0000

    Submit Form

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -13,6 +13,10 @@ const DEFAULT_DATA = {
 const ResourceCreate = () => {
   const [form, setForm] = useState(DEFAULT_DATA);
 
+  const submitForm = () => {
+    alert(JSON.stringify(form));
+  }
+
   return (
     <Layout>
       <div className="container">
@@ -75,7 +79,10 @@ const ResourceCreate = () => {
                 </div>
                 <div className="field is-grouped">
                   <div className="control">
-                    <button className="button is-link">Submit</button>
+                    <button
+                      type="button"
+                      onClick={submitForm}
+                      className="button is-link">Submit</button>
                   </div>
                   <div className="control">
                     <button className="button is-link is-light">Cancel</button>
