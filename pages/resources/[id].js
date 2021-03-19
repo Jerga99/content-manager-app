4cb0ea3a86baef8bfe3be7b764695b71e1b6e9e6
Filip Jerga <jerga99@gmail.com>
2021-03-19 08:50:22 +0000

    Resource detail page

---
+++ pages/resources/[id].js
@@ -0,0 +1,30 @@
+
+import Layout from "components/Layout";
+
+const ResourceDetail = () => {
+
+  return (
+    <Layout>
+      <section className="hero ">
+        <div className="hero-body">
+          <div className="container">
+            <section className="section">
+              <div className="columns">
+                <div className="column is-8 is-offset-2">
+                  <div className="content is-medium">
+                    <h2 className="subtitle is-4">DATE HERE</h2>
+                    <h1 className="title">TITLE HERE</h1>
+                    <p>DESC HERE</p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+
+export default ResourceDetail;
