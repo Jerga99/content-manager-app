7c8dc6b57cf253b42097dedfc0c743459ab7d6ef
Filip Jerga <jerga99@gmail.com>
2021-03-19 11:36:30 +0000

    Resource Edit page

---
+++ pages/resources/[id]/edit.js
@@ -0,0 +1,12 @@
+
+import Layout from "components/Layout";
+
+const ResourceEdit = () => {
+  return (
+    <Layout>
+      I am edit page!
+    </Layout>
+  )
+}
+
+export default ResourceEdit;
--- pages/resources/[id]/index.js
+++ pages/resources/[id]/index.js
@@ -1,13 +1,8 @@
 
 import Layout from "components/Layout";
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 const ResourceDetail = ({resource}) => {
-  const router = useRouter();
-
-  // if (router.isFallback) {
-  //   return <div>Loading Data!</div>;
-  // }
 
   return (
     <Layout>
@@ -21,6 +16,11 @@ const ResourceDetail = ({resource}) => {
                     <h2 className="subtitle is-4">{resource.createdAt}</h2>
                     <h1 className="title">{resource.title}</h1>
                     <p>{resource.description}</p>
+                    <Link href={`/resources/${resource.id}/edit`}>
+                      <a className="button is-warning">
+                        Update
+                      </a>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -32,31 +32,14 @@ const ResourceDetail = ({resource}) => {
   )
 }
 
-export async function getStaticPaths() {
-  const resData = await fetch("http://localhost:3001/api/resources");
-  const data = await resData.json();
-  const paths = data.map(resource => {
-    return {
-      params: { id: resource.id}
-    }
-  });
-
-  return {
-    paths,
-    // means that other routes should resolve into 404 page
-    fallback: false
-  }
-}
-
-export async function getStaticProps({params}) {
+export async function getServerSideProps({params}) {
   const dataRes = await fetch(`http://localhost:3001/api/resources/${params.id}`);
   const data = await dataRes.json();
 
   return {
     props: {
       resource: data
-    },
-    revalidate: 1
+    }
   }
 }
 
