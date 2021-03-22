36b3ff115433f07870879cdd16ecff5d44cbc4f5
Filip Jerga <jerga99@gmail.com>
2021-03-22 10:19:41 +0000

    Active resource patch req

--- pages/resources/[id]/index.js
+++ pages/resources/[id]/index.js
@@ -1,8 +1,15 @@
 
 import Layout from "components/Layout";
 import Link from "next/link";
+import axios from "axios";
 
 const ResourceDetail = ({resource}) => {
+
+  const activeResource = () => {
+    axios.patch("/api/resources", {...resource, status: "active"})
+      .then(_ => alert("Resource has been activated!"))
+      .catch(_ => alert("Cannot active the resource!"))
+  }
 
   return (
     <Layout>
@@ -16,11 +23,17 @@ const ResourceDetail = ({resource}) => {
                     <h2 className="subtitle is-4">{resource.createdAt}</h2>
                     <h1 className="title">{resource.title}</h1>
                     <p>{resource.description}</p>
+                    <p>Time to finish: {resource.timeToFinish} min</p>
                     <Link href={`/resources/${resource.id}/edit`}>
                       <a className="button is-warning">
                         Update
                       </a>
                     </Link>
+                    <button
+                      onClick={activeResource}
+                      className="button is-success ml-1">
+                      Activate
+                    </button>
                   </div>
                 </div>
               </div>
